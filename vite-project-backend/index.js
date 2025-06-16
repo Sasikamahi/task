77c196bb1a006068ca0046a333fe1c5d8d1b6574
Sasikamahi <152479283+Sasikamahi@users.noreply.github.com>
2025-06-16 10:50:03 +0530

    Add files via upload

---
+++ vite-project-backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mdb = require("mongoose");
+const signupController = require("./controllers/signup")
+const loginController = require("./controllers/login")
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors())
+dotenv.config()
+const PORT = process.env.PORT || 3000;
+
+mdb.connect(process.env.MONGO_URL).then(() => {
+    console.log("MongoDB Connection Successful");
+}).catch((err) => {
+    console.log("MongoDB Connection Unsuccessful",err);
+});
+
+app.get('/', (req, res) => {
+    res.send("Welcome to June Session of Backend Server");
+});
+app.use('/',signupController)
+app.use('/',loginController)
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running successfully on port ${PORT}`);
+})
